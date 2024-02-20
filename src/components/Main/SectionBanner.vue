8442f8377d8da4c3c7c3cad397d55c74b12dfa36
mas0110 <masoudmohammed13@gmail.com>
2024-02-20 17:49:18 +0100

    sezione banner pubblicitario

---
+++ src/components/Main/SectionBanner.vue
@@ -0,0 +1,36 @@
+<script>
+
+export default{
+    name:"SectionBanner",
+    components:{
+    }
+}
+</script>
+
+<template>
+<div class="bg">
+    <div class="container">
+       <figure>
+        <img src="../../assets/img/page-banner-1.webp" alt="">
+       </figure>
+    </div>
+ </div>
+</template>
+
+<style scoped lang="scss">
+.container{
+margin: 0 auto;
+width: 80%;
+img{
+width: 1150px;
+border-radius: 5px;
+}
+}
+figure{
+margin: 0px;}
+.bg{
+background-color:#F3F3F3 ;
+padding-block: 20px;
+margin-block: 50px;
+}
+</style>
