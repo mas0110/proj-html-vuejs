891c63a8ab39fb9a7c00dc1ebbcc21d6095cffae
mas0110 <masoudmohammed13@gmail.com>
2024-02-21 16:52:28 +0100

    testi sezione grid

--- src/components/Main/SectionGrid.vue
+++ src/components/Main/SectionGrid.vue
@@ -16,26 +16,29 @@ export default {
 
                 <div class="card-body">
                     <div class="text d-flex flex-column text-center">
-                        <h5 class="card-title">ciao</h5>
-                        <span class="opacity-50">ciao</span>
+                       <span>
+                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"/></svg>
+                       december 25, 2022
+                       </span>
+                       <h5 class="card-title">Fashion trend now a days</h5>
                     </div>
                     <div class="category">
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
+                        <a href="#">fashion</a>
+                        <a href="#">lifestyle</a>
                     </div>
                 </div>
             </div>
             <div class="side card bg-light text-center" style="width: 20rem; background-image: url(src/assets/img/travel-alone-300x200.webp); background-size:cover;">
                 <div class="card-body">
                     <div class="text d-flex flex-column text-center">
-                        <h5 class="card-title">ciao</h5>
-                        <span class="opacity-50">ciao</span>
+                        <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"/></svg>
+                        december 26, 2022</span>
+                        <h5 class="card-title">travelling alone is awesome</h5>
                     </div>
                     <div class="category">
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
+                        <a href="#">stories</a>
+                        <a href="#">travel</a>
                     </div>
                 </div>
             </div>
@@ -46,13 +49,15 @@ export default {
             <div class="main card bg-light text-center" style="width: 30rem; background-image: url(src/assets/img/best-places.webp); background-size:cover;">
                 <div class="card-body">
                     <div class="text d-flex flex-column text-center">
-                        <h5 class="card-title">ciao</h5>
-                        <span class="opacity-50">ciao</span>
+                        <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"/></svg>
+                        december 25, 2022</span>
+                        <h5 class="card-title">places for a road trip</h5>
                     </div>
                     <div class="category">
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
+                        <a href="#">lifestyle</a>
+                        <a href="#">stories</a>
+                        <a href="#">travel</a>
                     </div>
                 </div>
             </div>
@@ -64,13 +69,15 @@ export default {
             background-size:cover;">
                 <div class="card-body">
                     <div class="text d-flex flex-column text-center">
-                        <h5 class="card-title">ciao</h5>
-                        <span class="opacity-50">ciao</span>
+                        <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"/></svg>
+                        decembre 25, 2022</span>
+                        <h5 class="card-title">music the love of my life </h5>
                     </div>
                     <div class="category">
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
+                        <a href="#">culture</a>
+                        <a href="#">lifestyle</a>
+                      
                     </div>
                 </div>
             </div>
@@ -78,13 +85,14 @@ export default {
             background-size:cover;">
                 <div class="card-body">
                     <div class="text d-flex flex-column text-center">
-                        <h5 class="card-title">ciao</h5>
-                        <span class="opacity-50">ciao</span>
+                        <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"/></svg>
+                        december 26, 2022</span>
+                        <h5 class="card-title">reasons to visit france"</h5>
                     </div>
                     <div class="category">
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
-                        <a href="#">ciao</a>
+                        <a href="#">lifestyle</a>
+                        <a href="#">travel</a>
                     </div>
                 </div>
             </div>
@@ -93,16 +101,23 @@ export default {
 </template>
 
 <style scoped lang="scss">
-.card {
-    position: relative;
+.card-body {
+    display: flex;
+    justify-content: center;
+    align-items: end;
     .text {
-        position: absolute;
-        bottom: 10px;
-        right: 50%;
-        transform: translate(-50%);
+       color: white;
+       filter: none;
+       span svg{
+       fill: white;
+       height: 15px;
+       }
     }
 }
 .card:hover{
+filter: brightness(1);
+}
+.card{
 filter: brightness(0.7);
 }
 
